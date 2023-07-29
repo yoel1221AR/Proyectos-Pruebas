746abdd634cd1bad62735be9a8822deba86e5c4c
Yoel <64820937+yoel1221AR@users.noreply.github.com>
2023-07-29 12:53:45 -0300

    Add files via upload

---
+++ My-Shopping/src/common/header/Head.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label>+54 9245 492010</label>
+            <i className="fa fa-envelope"></i>
+            <label>yoelchinomandelli@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ"s</label>
+            <label>Need Helps</label>
+            <span></span>
+            <label>EN</label>
+            <span></span>
+            <label>USA</label>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
